59d0bcd169ec534c51d7b8f0163b895bc5155520
Tonhao Santos <ahssantos1@gmail.com>
2023-09-16 23:10:09 -0300

    feat: added chameleon grid layout

---
+++ packages/scss/chameleon-grid-layout/modules/index.js
@@ -0,0 +1,9 @@
+(function() {
+  // Creating the <link> for CSS file
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = './chameleon-grid-layout.min.css';
+
+  // Adding the <link> element to <head> element
+  document.head.appendChild(link);
+})()
