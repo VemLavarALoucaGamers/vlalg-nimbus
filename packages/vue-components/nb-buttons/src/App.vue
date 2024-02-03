6e3a8149e808956620eb00b14e80031122c22d7a
Tonhao Santos <ahssantos1@gmail.com>
2024-02-03 01:22:00 -0300

    feat: added 3 new vue-components buttons

--- packages/vue-components/nb-buttons/src/App.vue
+++ packages/vue-components/nb-buttons/src/App.vue
@@ -7,7 +7,95 @@
 				<p class="test-page__warning">Warning: Look at the console to see the click event</p>
 			</div>
 		</div>
+
 		<div class="row">
+			<div
+				class="col-xs-12 col-md-10 col-md-offset-1 test-page__content"
+				style="margin-bottom: 50px; overflow: hidden"
+			>
+				<select v-model="btType">
+					<option value="teste">teste</option>
+					<option value="showHover">showHover</option>
+					<option value="colorHover">colorHover</option>
+					<option value="mechanical">mechanical</option>
+				</select>
+			</div>
+		</div>
+
+		<div
+			v-if="btType === 'showHover'"
+			class="row"
+		>
+			<div
+				class="col-xs-12 col-md-10 col-md-offset-1 test-page__content"
+				style="margin-top: 50px; margin-bottom: 50px; overflow: hidden"
+			>
+				<h4 class="test-page__content-tile">NbButtonShowHover</h4>
+				<NbButtonShowHover
+					nb-id="NbButtonShowHover1"
+					text="button with block display"
+					container-color="#181818"
+					button-color="tomato"
+					text-color-hover="red"
+					button-color-hover="255,0,204"
+					verticalAlign="middle"
+					@clicked="buttonAction"
+				/>
+
+				<p style="margin-top: 4px">
+					Text before
+					<NbButtonShowHover
+						nb-id="NbButtonShowHover2"
+						text="button with inline-block display"
+						container-color="#181818"
+						button-color="tomato"
+						text-color-hover="red"
+						button-color-hover="255,0,204"
+						verticalAlign="middle"
+						@clicked="buttonAction"
+					/>
+					text after
+				</p>
+			</div>
+		</div>
+
+		<div
+			v-else-if="btType === 'colorHover'"
+			class="row"
+		>
+			<div
+				class="col-xs-12 col-md-10 col-md-offset-1 test-page__content"
+				style="margin-top: 50px; margin-bottom: 50px; overflow: hidden"
+			>
+				<h4 class="test-page__content-tile">NbButtonColorHover</h4>
+				<NbButtonColorHover
+					nb-id="NbButtonColorHover1"
+					display="b"
+					text="button with block display"
+					container-color="#181818"
+					button-color="tomato"
+					@clicked="buttonAction"
+				/>
+
+				<p style="margin-top: 4px">
+					Text before
+					<NbButtonColorHover
+						nb-id="NbButtonColorHover2"
+						display="ib"
+						text="button with inline-block display"
+						container-color="#181818"
+						button-color="tomato"
+						@clicked="buttonAction"
+					/>
+					text after
+				</p>
+			</div>
+		</div>
+
+		<div
+			v-else-if="btType === 'mechanical'"
+			class="row"
+		>
 			<div
 				class="col-xs-12 col-md-10 col-md-offset-1 test-page__content"
 				style="margin-top: 50px; margin-bottom: 50px; overflow: hidden"
@@ -42,10 +130,15 @@
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 
 const NbButtonMechanical = defineAsyncComponent(() => import('@components/NbButtonMechanical.vue'))
+const NbButtonColorHover = defineAsyncComponent(() => import('@components/NbButtonColorHover.vue'))
+const NbButtonShowHover = defineAsyncComponent(() => import('@components/NbButtonShowHover.vue'))
 
+const btType = ref('colorHover')
+
+// btType
 const buttonAction = () => {
 	console.log('aqui')
 }
