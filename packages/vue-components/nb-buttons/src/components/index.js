2802e9f3e853483ce1cf21cab11f248c0a025e26
Tonhao Santos <ahssantos1@gmail.com>
2024-03-03 02:06:50 -0300

    fix: added new buttons in the install config file

--- packages/vue-components/nb-buttons/src/components/index.js
+++ packages/vue-components/nb-buttons/src/components/index.js
@@ -1,15 +1,21 @@
-import NbButtonMechanical from '@components/NbButtonMechanical.vue';
-import NbButtonColorHover from '@components/NbButtonColorHover.vue';
-import NbButtonShowHover from '@components/NbButtonShowHover.vue';
-import NbButtonHamburger from '@components/NbButtonHamburger.vue';
 import NbButtonVavaGame from '@components/NbButtonVavaGame.vue';
+import NbButtonShowHover from '@components/NbButtonShowHover.vue';
 import NbButtonPrevNext from '@components/NbButtonPrevNext.vue';
+import NbButtonMechanical from '@components/NbButtonMechanical.vue';
+import NbButtonMechanical2 from '@components/NbButtonMechanical2.vue';
+import NbButtonHamburger from '@components/NbButtonHamburger.vue';
+import NbButtonColorOutside from '@components/NbButtonColorOutside.vue';
+import NbButtonColorHover from '@components/NbButtonColorHover.vue';
+import NbButtonAlternateText from '@components/NbButtonAlternateText.vue';
 
 export default {
-  NbButtonMechanical,
-  NbButtonColorHover,
-  NbButtonShowHover,
-  NbButtonHamburger,
   NbButtonVavaGame,
-  NbButtonPrevNext
+  NbButtonShowHover,
+  NbButtonPrevNext,
+  NbButtonMechanical,
+  NbButtonMechanical2,
+  NbButtonHamburger,
+  NbButtonColorOutside,
+  NbButtonColorHover,
+  NbButtonAlternateText
 };
