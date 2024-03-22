ea663b40c1f2245586318ddfc11a761a007ade50
Tonhao Santos <ahssantos1@gmail.com>
2024-03-21 23:22:37 -0300

    chore: added github star button in the doc

---
+++ apps/nimbus-docs/components/content/GithubProjectStar.client.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    Support leaving a
+    <a
+      class="github-button github-star-button"
+      href="https://github.com/VemLavarALoucaGamers/vlalg-nimbus"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Star VemLavarALoucaGamers/vlalg-nimbus on GitHub"
+    >
+      Star
+    </a>
+  </div>
+</template>
+
+<script setup>
+onBeforeMount(() => {
+  insertScript();
+});
+
+const insertScript = () => {
+  const script = document.createElement('script');
+
+  script.src = 'https://buttons.github.io/buttons.js';
+  script.async = true;
+  script.defer = true;
+
+  document.body.appendChild(script);
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  position: relative !important;
+
+  :deep(span) {
+    position: absolute;
+    margin-left: 8px;
+  }
+}
+</style>
