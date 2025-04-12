353c2fdf1df7495a8c6c23095d570ced1651e13b
TonhaoSantos <ahssantos1@gmail.com>
2025-04-12 01:43:48 -0300

    fix(project): removed trash

--- packages/vue-components/nb-test/src/main.js
+++ packages/vue-components/nb-test/src/main.js
@@ -1,18 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import ThemePlugin from './plugin/theme';
-import './plugin/theme.css';
-
 import './style/style.css';
-import './style/theme-change.css';
 // import '../../../scss/main.scss';
 
 const app = createApp(App)
-
-app.use(ThemePlugin, {
-  theme: 'dark',
-  dev: true,
-});
 
 app.mount('#app');
